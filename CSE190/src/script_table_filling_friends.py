a6a3f919c424cc03d03952ed80139db759cb7b6b
dhl013 <ldh4.ch@gmail.com>
2014-03-08 12:48:03 -0800

    Currently changing the script to work

--- CSE190/src/script_table_filling_friends.py
+++ CSE190/src/script_table_filling_friends.py
@@ -32,9 +32,9 @@ for i in range(0, numName):     # for each member
     fList = []      # Initialize the friend list
     
     for j in range(0, roundNumber):     # make (roundNumber) friends
-        currFriend = random.randrange(0, numName)
+        currFriend = random.randrange(0, numName)   # we use the id not name.
         
-        if(currFriend not in fList or currFriend != i):  # if already have this friend, run again
+        if(currFriend not in fList and currFriend != i):  # if already have this friend, run again
             cursor.execute("INSERT INTO friends VALUES ('" + str(i) + "', '" + str(currFriend) + "', NULL)")
         fList.append(currFriend)
             
